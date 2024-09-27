9ff2a872c906ed12de6c1ab54bae9552de4de2c2
ducsakun <vanducvt0305@gmail.com>
2024-09-27 20:47:58 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      backgroundColor: {},
+    },
+  },
+  plugins: [],
+};
